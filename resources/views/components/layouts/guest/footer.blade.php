b1faf263cdc694a104cd2f1f2905c382fe1013c4
dharmendra <dharmendrahubwale@gmail.com>
2025-06-23 11:48:23 +0000

    feat(guest layout): add guest layout components and structure
    - Added app.blade.php for guest layout structure
    - Added footer.blade.php for guest footer component
    - Added navbar.blade.php for guest navbar component

---
+++ resources/views/components/layouts/guest/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="w-full p-3 bg-white md:p-3 lg:p-3 dark:bg-gray-800 shadow">
+    <div class="mx-auto max-w-screen-xl text-center">
+        <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+            © {{ now()->format('Y') }} <a href="#" class="hover:underline">{{ config('app.name')}}™</a>. All Rights Reserved.
+        </span>
+    </div>
+</footer>
---
+++ resources/views/components/layouts/guest/navbar.blade.php
@@ -0,0 +1,11 @@
+{{-- <nav class="bg-white shadow-lg sticky top-0 z-50" x-data="{ open: false }">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="flex justify-between h-16">
+            <div class="flex items-center">
+                <a href="#" class="text-xl font-bold text-gray-800">
+                    <img src="{{ asset('icon/hubwalelogopng.png') }}" alt="Logo" class="h-10 w-auto">
+                </a>
+            </div>
+        </div>
+    </div>
+</nav> --}}
