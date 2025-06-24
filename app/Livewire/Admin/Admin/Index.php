62e75431488be12101b3e64f74f525a027557432
rutvi <rutvihubwale@gmail.com>
2025-06-24 06:54:33 +0000

    feat(admin/plans): Implement CRUD functionality for Plans management
    - Added Plan creation component with form validation and success message on create.
    - Added Plan edit component skeleton.
    - Added Plan index component with pagination and delete confirmation.
    - Updated routes for Plan management.
    - Enhanced UI elements for better user experience.

--- app/Livewire/Admin/Admin/Index.php
+++ app/Livewire/Admin/Admin/Index.php
@@ -4,19 +4,12 @@ namespace App\Livewire\Admin\Admin;
 
 use Livewire\Attributes\Layout;
 use Livewire\Component;
-use App\Models\User;
-use Livewire\WithPagination;
 
 class Index extends Component
 {
-    use WithPagination;
-
-    public $user;
     #[Layout('components.layouts.superadmin.app')]
     public function render()
     {
-        return view('livewire.admin.admin.index', [
-            'users' => User::paginate(10),
-        ]);
+        return view('livewire.admin.admin.index');
     }
 }
