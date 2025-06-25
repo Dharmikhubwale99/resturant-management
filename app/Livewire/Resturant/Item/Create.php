151746ef22679f257d91e1414cba98df6741130f
rutvi <rutvihubwale@gmail.com>
2025-06-25 12:45:07 +0000

    refactor(Livewire/Resturant/Item): Refactor Livewire components for restaurant items
    - Added Layout attribute to Create, Edit, and Index components.
    - Improved restaurant item retrieval by filtering based on authenticated user's restaurant.

--- app/Livewire/Resturant/Item/Create.php
+++ app/Livewire/Resturant/Item/Create.php
@@ -21,7 +21,6 @@ class Create extends Component
 
     #[Layout('components.layouts.resturant.app')]
 
-        // Livewire component
     public function mount(): void
     {
         $this->restaurant = auth()->user()->restaurants()->first();
@@ -30,7 +29,7 @@ class Create extends Component
         $this->categories = $this->restaurant
                                 ->categories()
                                 ->orderBy('name')
-                                ->pluck('name', 'id')   // ← already an array
+                                ->pluck('name', 'id')  
                                 ->toArray();
 
         // enum → array: ['non_veg' => 'Non-Veg', …]
