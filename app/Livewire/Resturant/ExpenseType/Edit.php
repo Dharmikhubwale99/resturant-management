5e7fcf7097eb9e50bdc6c77ec86b1f9afe2011d2
dharmendra <dharmendrahubwale@gmail.com>
2025-07-02 11:49:58 +0000

    feat(expenses): Add expense management features including create, edit, delete, and list functionality for expenses and expense types.
    - Introduced Livewire components for managing expenses and expense types.
    - Enhanced UI for creating and editing expenses and expense types.
    - Implemented deletion confirmation dialogs for expense types and expenses.

--- app/Livewire/Resturant/ExpenseType/Edit.php
+++ app/Livewire/Resturant/ExpenseType/Edit.php
@@ -2,12 +2,36 @@
 
 namespace App\Livewire\Resturant\ExpenseType;
 
+use App\Models\ExpenseType;
 use Livewire\Component;
+use Livewire\Attributes\Layout;
 
 class Edit extends Component
 {
+     public $name, $expense_type;
+
+    #[Layout('components.layouts.resturant.app')]
     public function render()
     {
         return view('livewire.resturant.expense-type.edit');
+    }
+
+    public function mount($id) 
+    {
+        $this->expense_type = ExpenseType::find($id);
+        $this->fill($this->expense_type->only('name'));
+    }
+
+    public function submit()
+    {
+        $this->validate([
+            'name' => 'required|unique:expense_types,name,' . $this->expense_type->id . ',id,restaurant_id,' . $this->expense_type->restaurant_id,
+        ]);
+
+        $this->expense_type->update([
+            'name' => $this->name,
+        ]);
+
+        return redirect()->route('restaurant.expense-types.index')->with('success', 'Expense type updated successfully.');
     }
 }
