d55ab81c8a520481a6130631490d2be6af8535e1
dharmendra <dharmendrahubwale@gmail.com>
2025-08-06 12:57:11 +0530

    Plan design set

--- app/Livewire/Resturant/PlanPurchase.php
+++ app/Livewire/Resturant/PlanPurchase.php
@@ -4,16 +4,22 @@ namespace App\Livewire\Resturant;
 
 use Livewire\Component;
 use Livewire\Attributes\Layout;
-use App\Models\Plan;
+use App\Models\{Plan, AppConfiguration, PlanFeature};
 
 class PlanPurchase extends Component
 {
     #[Layout('components.layouts.auth.app')]
     public function render()
     {
-        $plan = Plan::where('is_active',0)->get();
-        return view('livewire.resturant.plan-purchase',[
-            'plans' => $plan
+        $plans = Plan::where('is_active', 0)->get();
+        $modules = AppConfiguration::all();
+
+        $planFeatures = PlanFeature::all()->groupBy('plan_id');
+
+        return view('livewire.resturant.plan-purchase', [
+            'plans' => $plans,
+            'modules' => $modules,
+            'planFeatures' => $planFeatures
         ]);
     }
 }
